213e7f80d1250a47649b83d7c1f9948ebba9fa8a
Semon Albert <semonalbert@yeah.net>
2017-04-13 17:21:52 +0800

    add: dict_util:filter_dict

--- pyutils/dict_util.py
+++ pyutils/dict_util.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+import copy
 
 def is_subdict(con, sub):
     for key, value in sub.iteritems():
@@ -27,3 +28,17 @@ def dict_has_keys(d, keys):
 
 def sub_dict(d, *keys):
     return dict((key, d[key]) for key in keys if key in d)
+
+
+def filter_dict(d, filter_cond=None):
+    """
+        create a new sub-dict from d whose memeber is filtered by filter_cond
+        input:
+            d -> source dict
+            filter_cond -> function(key, value) -> bool
+    """
+    d = dict(d)
+    if filter_cond:
+        return {key: val for key, val in d.iteritems() if filter_cond(key, val)}
+    else:
+        return copy.copy(d)
